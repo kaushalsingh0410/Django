d92012d231f28ea131c7f16fc415eb56cda38711
kaushalsingh0410 <kaushalsingh@gmail.com>
2025-06-01 18:51:14 +0530

    dynamic crud complited

--- testProject/app/urls.py
+++ testProject/app/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index,name='index'),
+    path('<int:pk>/', index,name='index-update'),
+    path('<int:pk>/', index,name='index-update'),
+    path('delete/', delete,name='delete'),
+    path('table/', table,name='table'),
 ]
--- testProject/app/views.py
+++ testProject/app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse,JsonResponse
 from .form import UserForm
 from .models import User
 from django.template.loader import render_to_string
-def index(request):
+def index(request,pk = None):
 
     data = {
         'title':'Index',
@@ -12,13 +12,35 @@ def index(request):
         }
     print('inside index request',request.method)
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        comment = request.POST.get('comment')
-        user = User.objects.create(name =name,email = email,comment = comment)
-        
-        tableHtml = render_to_string('app/table.html',{'query':[user]})
-        return JsonResponse({'msg':'Good',"tableHtml":tableHtml})
+        form = None
+        if pk:
+            user = User.objects.get(pk =pk)
+            form = UserForm(request.POST,instance = user)
+            print('This is update')
+            # name = request.POST.get('name')
+            # email = request.POST.get('email')
+            # comment = request.POST.get('comment')
+            # user = User.objects.create(name =name,email = email,comment = comment)
+            
+            # tableHtml = render_to_string('app/table.html',{'query':[user]})
+            # return JsonResponse({'msg':'Good',"tableHtml":tableHtml})
+            # return JsonResponse({'msg':'update'})
+        else:
+            form = UserForm(request.POST)
+            # name = request.POST.get('name')
+            # email = request.POST.get('email')
+            # comment = request.POST.get('comment')
+            # user = User.objects.create(name =name,email = email,comment = comment)
+        if form.is_valid():
+            user = form.save()
+            
+            tableHtml = render_to_string('app/table.html',{'query':[user]})
+            return JsonResponse({'msg':'Good',
+                                 'id':user.id,
+                                 "tableHtml":tableHtml,
+                                 'update':pk is not None})
+          
+
 
 
 
@@ -28,3 +50,15 @@ def index(request):
 
     return render(request,'app/index.html',data)
 
+
+
+def delete(request):
+    if request.method == 'POST':
+        print('this is delete',request.POST)
+        User.objects.get(pk = request.POST.get('pk')).delete()
+        return JsonResponse({'msg':True})
+
+
+def table(request):
+    # return render(request,'app/tableView.html'  )
+    return render(request,'app/tableView.html',{'tableHtml':render_to_string('app/table.html',{'query':User.objects.all()})})
