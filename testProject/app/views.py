456474d8bcd638f4168f66f7718bf7df6197c183
kaushalsingh0410 <kaushalsingh@gmail.com>
2025-06-01 12:34:46 +0530

    ajax added to for and make table dynamic using render_to_string

--- testProject/app/views.py
+++ testProject/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .form import UserForm
+from .models import User
+from django.template.loader import render_to_string
 def index(request):
 
     data = {
@@ -13,9 +15,16 @@ def index(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         comment = request.POST.get('comment')
-        print('name',name)
-        print('email',email)
-        print('comment',comment)
-        return JsonResponse({'msg':'Good'})
+        user = User.objects.create(name =name,email = email,comment = comment)
+        
+        tableHtml = render_to_string('app/table.html',{'query':[user]})
+        return JsonResponse({'msg':'Good',"tableHtml":tableHtml})
+
+
+
+        
+    data['tableHtml'] = render_to_string('app/table.html',{"query":User.objects.order_by('-id')})
+    # print('table',data['table'],type(data['table']),len(data['table']))
+
     return render(request,'app/index.html',data)
 
