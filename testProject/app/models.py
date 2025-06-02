2aaf83d78dc4601d51f4eae0b82e05d0e07d0d03
kaushalsingh0410 <kaushalsingh@gmail.com>
2025-06-02 09:39:53 +0530

    add tinymce into admin for comment field

--- testProject/app/models.py
+++ testProject/app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class User(models.Model):
     name = models.CharField(max_length= 50)
     email = models.EmailField(max_length= 50)
-    comment = models.TextField(max_length= 50)
+    comment = HTMLField()
 
     def __str__(self):
         return self. name
